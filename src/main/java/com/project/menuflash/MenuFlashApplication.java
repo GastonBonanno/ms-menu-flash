7cdc828eadea2ea51eaf2509789416981b9f0bad
Paula <paulalezcano21@gmail.com>
2023-03-09 00:02:51 -0300

    first commit

---
+++ src/main/java/com/project/menuflash/MenuFlashApplication.java
@@ -0,0 +1,13 @@
+package com.project.menuflash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MenuFlashApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MenuFlashApplication.class, args);
+	}
+
+}

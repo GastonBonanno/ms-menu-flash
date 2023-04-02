1ba81c7f4f2f11ae5eb142a101db43f6774b3a81
Gastón Bonanno <gbonanno-dev@gmail.com>
2023-04-02 15:56:48 -0300

    feat(MF-02): Add log4j, add create endpoint

---
+++ src/main/java/com/project/menuflash/dto/response/CreateStateResponseDto.java
@@ -0,0 +1,8 @@
+package com.project.menuflash.dto.response;
+
+import lombok.Data;
+
+@Data
+public class CreateStateResponseDto {
+    private String name;
+}
--- src/main/java/com/project/menuflash/service/state/StateServiceImpl.java
+++ src/main/java/com/project/menuflash/service/state/StateServiceImpl.java
@@ -1,36 +1,53 @@
 package com.project.menuflash.service.state;
 
-import com.project.menuflash.domain.StateDomain;
-import com.project.menuflash.dto.StateDTO;
+import com.project.menuflash.controller.StateController;
+import com.project.menuflash.dto.request.CreateStateDto;
+import com.project.menuflash.dto.request.UpdateStateDto;
+import com.project.menuflash.dto.response.UpdateStateResponseDto;
 import com.project.menuflash.entity.StateEntity;
 import com.project.menuflash.repository.StateRepository;
 import org.springframework.stereotype.Service;
 
+import javax.swing.plaf.nimbus.State;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
 public class StateServiceImpl implements StateService{
+
+    private static final org.apache.logging.log4j.Logger LOG = org.apache.logging.log4j.LogManager.getLogger(StateController.class);
     private final StateRepository stateRepository;
+
 
     public StateServiceImpl(StateRepository stateRepository) {
         this.stateRepository = stateRepository;
     }
 
-    public List<StateDomain> getStates() throws Exception {
+    public List<UpdateStateResponseDto> getStates() throws Exception {
         List<StateEntity> stateEntities = stateRepository.findAll();
-        return stateEntities.stream().map(StateEntity::toDomain).collect(Collectors.toList());
+        return stateEntities.stream().map(StateEntity::toResponseDto).collect(Collectors.toList());
     }
 
-    public StateDomain getStateById(Long id) throws Exception {
+    public UpdateStateResponseDto getStateById(Long id) throws Exception {
         StateEntity stateIdEntity = stateRepository.findById(id).get();
-        return stateIdEntity.toDomain();
+        return stateIdEntity.toResponseDto();
     }
 
-    public void updateState(StateDTO stateDto) throws Exception {
+    public void createState(CreateStateDto createStateDTO) throws Exception {
         try {
-            StateEntity stateEntity = getStateEntityById(stateDto.getId());
-            stateRepository.save(stateEntity.updateFromDto(stateDto));
+            StateEntity entity = new StateEntity();
+            entity.setName(createStateDTO.getName());
+            stateRepository.save(entity);
+        } catch (Exception e) {
+            LOG.error("Error: {}", e.getMessage());
+            throw new Exception("Update entity error");
+        }
+    }
+
+    public void updateState(UpdateStateDto updateStateDto) throws Exception {
+        try {
+            StateEntity stateEntity = getStateEntityById(updateStateDto.getId());
+            stateRepository.save(stateEntity.updateFromDto(updateStateDto));
         } catch (Exception e) {
             throw new Exception("Update entity error");
         }

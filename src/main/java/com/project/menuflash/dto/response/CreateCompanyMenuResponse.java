fce0b5e1e195dfbec2073993237a26231db32aa3
ACarballal7744 <alan.carba@hotmail.com>
2023-08-27 16:14:19 -0300

    chore: WIP menu - category - item

---
+++ src/main/java/com/project/menuflash/dto/response/CreateCompanyMenuResponse.java
@@ -0,0 +1,21 @@
+package com.project.menuflash.dto.response;
+
+import lombok.Data;
+
+import java.util.Date;
+import java.util.List;
+
+@Data
+public class CreateCompanyMenuResponse {
+    private Long id;
+    private String title;
+    private String description;
+    private String header;
+    private String footer;
+    private Long companyDataId;
+    private Boolean active;
+    private Date createdAt;
+    private Date modifiedAt;
+    private Date deletedAt;
+}
+
